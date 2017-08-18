23452f571f8a752d2d73a35c960cdaf892b66016
Jin Soo Han <han3751@gmail.com>
2017-08-18 09:42:41 -0400

    End of day 3

--- julia/julia_intro.jl
+++ julia/julia_intro.jl
@@ -22,7 +22,7 @@ sphere_vol(3);
 
 # 2. String
 # Set path using string variables
-homedir();
+homedir()
 Path="/Dropbox/CompEcon_class/julia/";
 cd(string(homedir(),Path));
 pwd()
@@ -34,43 +34,56 @@ pwd()
 # 3. Arrays
 # arrays can be initialised and edited directly:
 a1 = [1, 2, 3];
+a1 = [1; 2; 3];
+a1 = [1 2 3];
+b1 = [1 2 3; 4 5 6]
+c1 = [a1; b1];
 println(a1)
 
 # commans are for enumeration and semi-colons are for concatenation
-a2 = [a1, 4];
+a2 = [a1; 4]
 println(a2)
-size(a2)
+size(b1)
 a3 = [a1; 4];
 println(a3)
 size(a3)
 a4=rand(2,2);
 size(a4)
-a5=rand(1,2,3);
-size(a5,1)
+a5=rand(1,2,3)
+size(a5,3)
+whos()
+a5=[]
 
 # arrays can also be generated from comprehensions:
  #try to use comprehensions as they are super-fast
-a4 = [i for i = 1:2:10];
+a4= [1, 3, 5, 7, 9]
+a4 = [i for i = 1:2:10]'
+a4 =inv(eye(2))
 println(a4)
 size(a4)
 
 # matrix & matrix operations
 m1=eye(2);
 m1[2,1]=2; # assign a new value to a certain cell
-println(m1)
+m1
 
 maximum(m1,1)    # find the max elt along dim 1
+
+m1[1,:]
+
+
 sum(m1,2)  # takes the sum over the second dimension (i.e., row)
-m2 = m1 .+ 1; # add 1 to all elements; "." is an element-wise operation
-println(m2)
+m2 = m1 .+ 1;
+m3 = m1 .+ [1 2]
+
+ # add 1 to all elements; "." is an element-wise operation
+m2
 
 
 
 # 4. Loops and Map
 # for loops:
-for i in 1:5;
- print(i, ", ");
-end;
+for i in 1:5; print(i, ", "); end;
 
 # while loops:
 i=1;
@@ -80,13 +93,14 @@ while i<=5;
 end;
 
 # like python enumerate can be used to get both the index and value in a loop:
-a1 = ["one", "two", "three"];
-for (i, v) in enumerate(a1);
- print(i, ": ", v, ", "); # Note that Julia index starts at 1 not 0
+a1 = ["A" "B"; "C" "D"];
+for (iter, value) in enumerate(a1[1,:]);
+ print(iter, ": ", value, ", "); # Note that Julia index starts at 1 not 0
 end;
 
 # map works by applying a function on each value of an array
-a2=[]; @time for i=1:5; a2=[a2; sphere_vol(i)] end;
+a2=zeros(5);
+@time for i=1:5; a2[i]=sphere_vol(i) end;
 @time a3 = map(sphere_vol, [1,2,3,4,5]);
 @time a4 = [sphere_vol(1),sphere_vol(2),sphere_vol(3),sphere_vol(4),sphere_vol(5)];
 a5 = map(x -> x^2, [1,2,3,4,5]); #using map with an anoymous function
@@ -97,9 +111,10 @@ println(a5)
 # 5. Packages
 # using pacakages saves time
 Pkg.add("Calculus")
+Pkg.status("Calculus")
 Pkg.update()
 using Calculus;
-derivative(x -> sin(x), 1.0)
+derivative(x -> f(x,y), [1.0 1.0])
 cos(1)
 
 # using optim package
@@ -110,18 +125,20 @@ cos(1)
  introduced by Howard H. Rosenbrock in 1960.
  It is also known as Rosenbrock's valley or Rosenbrock's banana function.
 =#
+Pkg.add("Optim")
 using Optim; #detailed manual @ http://julianlsolvers.github.io/Optim.jl/stable/
-rosenbrock(x) =  (1.0 - x[1])^2 + 100.0 * (x[2] - x[1]^2)^2;
-result = optimize(rosenbrock, zeros(2), BFGS());
-result = optimize(rosenbrock, [0.0, 0.0], NelderMead());
+y=0
+rosenbrock(x,y) =  (1.0 - x[1])^2 + 100.0 * (x[2] - x[1]^2)^2;
+result = optimize(x->rosenbrock(x,y), zeros(2), BFGS())
+result = optimize(rosenbrock, [0.0, 0.0], NelderMead())
 result = optimize(rosenbrock, [0.0, 0.0],BFGS(),
-               Optim.Options(g_tol = 1e-12,iterations = 10,store_trace = true,show_trace = true));
-@show result
+               Optim.Options(g_tol = 1e-12,iterations = 1000,store_trace = true,show_trace = true));
+result
 function g(storage, x);
  storage[1] = -2.0 * (1.0 - x[1]) - 400.0 * (x[2] - x[1]^2) * x[1];
  storage[2] = 200.0 * (x[2] - x[1]^2);
 end;
-result = optimize(rosenbrock, g, [0.0, 0.0], LBFGS()));
+result = optimize(rosenbrock, g, [0.0, 0.0], LBFGS())
 @show result
 function h(storage, x);
  storage[1, 1] = 2.0 - 400.0 * x[2] + 1200.0 * x[1]^2;
@@ -129,7 +146,7 @@ function h(storage, x);
  storage[2, 1] = -400.0 * x[1];
  storage[2, 2] = 200.0;
 end;
-result = optimize(rosenbrock, g, h, [0.0, 0.0], LBFGS());
+result = optimize(rosenbrock, g, h, [0.0, 0.0], LBFGS())
 @show result
 Optim.minimizer(result);
 
@@ -138,7 +155,7 @@ Optim.minimizer(result);
 # 6. Plots
 # This section only works in Atom
 using Plots;
-plot([cumsum(rand(500).-0.5) cumsum(rand(500).-0.5)]);
+plot([cumsum(rand(500).-0.5) cumsum(rand(500).-0.5)])
 
 
 
@@ -150,13 +167,14 @@ plot([cumsum(rand(500).-0.5) cumsum(rand(500).-0.5)]);
 # Let's add some processors ("workers")
 addprocs(4);
 nprocs()
+#rmprocs(4)
 
 # remotecall is a function to call a specific worker
 r = remotecall(rand, 2, 3, 3); # remotecall is telling worker 2 to create 3x3 random matrix
 println(r)
 
 # spawn is a function to call a worker
-r = @spawn rand(3, 3); # spawn is telling a worker to create 3x3 random matrix
+r = @spawn rand(1000000000000, 1000000000000); # spawn is telling a worker to create 3x3 random matrix
 println(r)
 
 # fetch sends the work back to the master
@@ -228,7 +246,7 @@ end;
 pi
 
 # Now define findpi in every processor to parallelize the calculation
-@everywhere function find_n_circle(n_total);
+function find_n_circle(n_total);
  n_circle=0;
  for i=1:n_total;
   x,y = rand(2)
@@ -238,9 +256,12 @@ pi
  end;
 return n_circle;
 end;
+tic()
 a=@spawn find_n_circle((10^8)/2);
 b=@spawn find_n_circle((10^8)/2);
+toc()
 4*(fetch(a)+fetch(b))/10^8
+
 # This example demonstrates a powerful and often-used parallel programming pattern.
 # Many iterations run independently(!) over several processes,
 # and then their results are combined using some function.
@@ -261,7 +282,7 @@ end;
 # in the previous example, @parallel assigins the for loop to different processors
 # and collect the results at the end using the (+) reduction
 # but if one wants each processor to calculate find_n_circle(N), then use pmap
-ans=pmap(find_n_circle, [(10^8)/2, (10^8)/2]);
+@time ans=pmap(find_n_circle, [(10^8)/2, (10^8)/2])
 4*sum(ans)/10^8
 # Julia's pmap() is designed for the case where each function call does a large amount of work.
 # In contrast, @parallel for can handle situations where each iteration is tiny,
