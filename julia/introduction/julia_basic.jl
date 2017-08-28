9f4af86034b792ce68ca3f7a0a392846299eb42d
Jin Soo Han <han3751@gmail.com>
2017-08-28 16:33:28 -0400

    updated for IO mini lecture 6

--- julia/introduction/julia_basic.jl
+++ julia/introduction/julia_basic.jl
@@ -6,6 +6,8 @@
 # 0. Introduction
 println("Hello. ","你好! ","안녕하세요?");
 println(nprocs());
+# Atom is useful because... it allows efficient editing
+# for e.g., multiple line editing: shift+cmd+L
 
 
 
@@ -20,7 +22,7 @@ sphere_vol(3);
 
 
 
-# 2. String
+# 2. Directories
 # Set path using string variables
 homedir()
 Path="/Dropbox/CompEcon_class/julia/";
@@ -33,57 +35,35 @@ pwd()
 
 # 3. Arrays
 # arrays can be initialised and edited directly:
-a1 = [1, 2, 3];
-a1 = [1; 2; 3];
-a1 = [1 2 3];
+a1 = [1; 2; 3]
+a1 = [1 2 3]
 b1 = [1 2 3; 4 5 6]
-c1 = [a1; b1];
-println(a1)
-
-# commans are for enumeration and semi-colons are for concatenation
-a2 = [a1; 4]
-println(a2)
-size(b1)
-a3 = [a1; 4];
-println(a3)
-size(a3)
-a4=rand(2,2);
-size(a4)
-a5=rand(1,2,3)
-size(a5,3)
-whos()
-a5=[]
+c1 = [a1; b1]
+c1[1,2]=5
+c1
 
 # arrays can also be generated from comprehensions:
- #try to use comprehensions as they are super-fast
-a4= [1, 3, 5, 7, 9]
-a4 = [i for i = 1:2:10]'
-a4 =inv(eye(2))
-println(a4)
-size(a4)
+a4= [1; 3; 5; 7; 9; 11; 13; 15]
+a4 = [i for i = 1:2:15]
 
-# matrix & matrix operations
-m1=eye(2);
-m1[2,1]=2; # assign a new value to a certain cell
+# matrix & operations & index
+m1=zeros(3,3);
+m1[1,2]=1;
+m1[2,2]=2;
 m1
-
-maximum(m1,1)    # find the max elt along dim 1
-
-m1[1,:]
-
-
-sum(m1,2)  # takes the sum over the second dimension (i.e., row)
-m2 = m1 .+ 1;
-m3 = m1 .+ [1 2]
-
- # add 1 to all elements; "." is an element-wise operation
+sum(m1,1) # takes the sum over the first dim
+maximum(m1,2) # find the max over the 2nd dim
+m1[1:2,:] # grabs the rows from 1 to 2 and grab all the columns
+m2 = m1 .+ 1 # add 1 to all elements; "." is an element-wise operation
 m2
 
 
 
 # 4. Loops and Map
 # for loops:
-for i in 1:5; print(i, ", "); end;
+for i in 1:5;
+  print(i, ", ");
+end;
 
 # while loops:
 i=1;
@@ -92,27 +72,19 @@ while i<=5;
  i+=1; # Shorthand for i = i + 1
 end;
 
-# like python enumerate can be used to get both the index and value in a loop:
-a1 = ["A" "B"; "C" "D"];
-for (iter, value) in enumerate(a1[1,:]);
- print(iter, ": ", value, ", "); # Note that Julia index starts at 1 not 0
-end;
-
 # map works by applying a function on each value of an array
-a2=zeros(5);
-@time for i=1:5; a2[i]=sphere_vol(i) end;
-@time a3 = map(sphere_vol, [1,2,3,4,5]);
-@time a4 = [sphere_vol(1),sphere_vol(2),sphere_vol(3),sphere_vol(4),sphere_vol(5)];
-a5 = map(x -> x^2, [1,2,3,4,5]); #using map with an anoymous function
-println(a5)
+# useful if functions are expensive
+@time a2=zeros(5); @time for i=1:5; a2[i]=sphere_vol(i) end; # 10 allocations
+@time a3 = [sphere_vol(1),sphere_vol(2),sphere_vol(3),sphere_vol(4),sphere_vol(5)]; # 5 allocations
+@time a4 = map(sphere_vol, [1,2,3,4,5]); # 7 allocations
 
 
 
 # 5. Packages
 # using pacakages saves time
 Pkg.add("Calculus")
-Pkg.status("Calculus")
 Pkg.update()
+Pkg.status("Calculus")
 using Calculus;
 derivative(x -> f(x,y), [1.0 1.0])
 cos(1)
@@ -165,62 +137,45 @@ plot([cumsum(rand(500).-0.5) cumsum(rand(500).-0.5)])
 # A remote call is a request by one process to call a certain function on certain arguments on another (possibly the same) process.
 
 # Let's add some processors ("workers")
-addprocs(4);
+addprocs(2);
 nprocs()
 #rmprocs(4)
 
-# remotecall is a function to call a specific worker
-r = remotecall(rand, 2, 3, 3); # remotecall is telling worker 2 to create 3x3 random matrix
-println(r)
-
-# spawn is a function to call a worker
-r = @spawn rand(1000000000000, 1000000000000); # spawn is telling a worker to create 3x3 random matrix
-println(r)
-
-# fetch sends the work back to the master
-s = r.+1; # Note that this will not run because data has not been fetched back to the master
-s = fetch(r).+1;
-
-# sending data requires some planning
-# method 1
-A = rand(1000,1000);
-A2 = @spawn A^2;
-fetch(A2);
-# method 2
-B = @spawn rand(1000,1000)^2;
-fetch(B)
-# Both methods do the same task, but the method 2 moves the data once
-# But, you could also imagine a case where method 1 is preferred (i.e., the master absolutely needs the matrix A)
-
-# check where things are
-C = rand(1000,1000)^2;
-remotecall_fetch(()->C, 2);
-@spawnat 2 whos()
-
 # Note that where you have the data matters
-# suppose you want to add 1 through 50
-a=[0];
-for i = 1:100;
-  a[1]+=i;
+# suppose you want to construct an array [100, 200, 300, 400, 500]
+a=zeros(5);
+for i = 1:5;
+  a[i]=i*100;
 end;
 println(a);
-# suppose you want do the addition parallelly
-@everywhere a=[0];
-@parallel for i = 1:100;
-    a[1]+=i;
+# suppose you want do construct the same array parallelly
+@everywhere a=zeros(5);
+@parallel for i = 1:5;
+    a[i]=i*100;
 end;
-@spawnat 2 println(a)
-@spawnat 3 println(a)
+println(a);
+@spawnat 2 println(a);
+@spawnat 3 println(a);
 # Not what you want...
-# you need to either call in all the different "a"'s generated in different processors
-# OR use Julia's SharedArray
-a = SharedArray{Int64}(1);
-@parallel for i = 1:100;
-    a[1]+=i;
-end;
-a
+# This happened because iterations run on different processors and do not happen in a specified order
+# Any variables used inside the parallel loop will be copied and broadcast to each processor
 
-# Let's find π using Monte Carlo
+
+# you can manually call in all the different "a"'s generated in different processors
+# OR use Julia's SharedArray
+a = SharedArray{Int64}(zeros(5));
+@parallel for i = 1:5;
+    a[i]=i*100;
+end;
+println(a);
+
+# Using Pmap for more expensive reduction operations
+a =0; for i=1:3; a+=i^2; end;
+a = @parallel (+) for i=1:3; i^2; end; # this calls the function
+a = sum(pmap(x->x^2,[1,2,3])); # this calls the function x->x^2 once
+
+
+# Example: If time permits... Let's find π using Monte Carlo
 #= theory:
 Let's imagine a circle with r=1 in a 2 by 2 square.
 We know that (area of circle / area of square) = π/4.
